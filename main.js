a61bb22b1a153588ed8190a1989ae4aec8d71510
Nikoolaii <nikolailemerre@gmail.com>
2025-02-07 14:36:14 +0100

    update

--- main.js
+++ main.js
@@ -1,60 +1,78 @@
-const folder = "./www"
+const folder = "./www";
 
 function listen(port) {
-  const net = require('net')
-  const server = net.createServer((socket) => {
-    console.log("New connection " + socket.remoteAddress + ":" + socket.remotePort)
-    socket.on('data', (data) => {
-      manageRequest(data.toString(), socket)
+  while (true) {
+    const net = require('net');
+    const server = net.createServer((socket) => {
+      console.log("New connection " + socket.remoteAddress + ":" + socket.remotePort);
+      socket.on('data', (data) => {
+        manageRequest(data.toString(), socket);
+      });
     })
-  })
-
-  server.listen(port, () => {
-    console.log(`Server listening on port ${port}`)
-  })
+  }
 }
 
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
+
 function manageRequest(request, socket) {
-  const requestLine = request.split('\n')[0]
-  const method = requestLine.split(' ')[0]
-  let path = requestLine.split(' ')[1]
+  const requestLine = request.split('\n')[0];
+  const method = requestLine.split(' ')[0];
+  let path = requestLine.split(' ')[1];
 
   if (path === '/') {
-    path = '/index.html'
+    path = '/index.html';
   }
 
   switch (method) {
     case "GET":
-      const file = (folder + path).trim()
-      const fs = require('fs')
-      console.log(file)
+      const file = (folder + path).trim();
+      const fs = require('fs');
+      console.log(file);
 
       try {
         if (fs.existsSync(file)) {
-          const fileContent = fs.readFileSync(file, '')
-          const extension = path.split('.').pop().toLowerCase()
-          const contentType = 'text/html'
+          const fileContent = fs.readFileSync(file);
+          const extension = path.split('.').pop().toLowerCase();
+          const contentType = getContentType(extension);
 
           const returnValue = [
             "HTTP/1.1 200 OK",
             `Content-Type: ${contentType}`,
             "",
             fileContent
-          ]
-          socket.write(returnValue.join('\r\n'))
+          ];
+          socket.write(returnValue.join('\r\n'));
         } else {
-          console.log('File not found:', file)
-          sendError(socket, 404, 'Not Found')
+          console.log('File not found:', file);
+          sendError(socket, 404, 'Not Found');
         }
       } catch (err) {
-        console.error('Error reading file:', err)
-        sendError(socket, 500, 'Internal Server Error')
+        console.error('Error reading file:', err);
+        sendError(socket, 500, 'Internal Server Error');
       }
-      break
+      break;
     default:
-      sendError(socket, 405, 'Method Not Allowed')
-      break
+      sendError(socket, 405, 'Method Not Allowed');
+      break;
   }
+  socket.end();
+}
+
+function getContentType(extension) {
+  const mimeTypes = {
+    'html': 'text/html',
+    'css': 'text/css',
+    'js': 'application/javascript',
+    'json': 'application/json',
+    'png': 'image/png',
+    'jpg': 'image/jpeg',
+    'gif': 'image/gif',
+    'txt': 'text/plain'
+  };
+  return mimeTypes[extension] || 'application/octet-stream';
 }
 
 function sendError(socket, code, message) {
@@ -63,8 +81,8 @@ function sendError(socket, code, message) {
     "Content-Type: text/html",
     "",
     `<h1>${code} - ${message}</h1>`
-  ]
-  socket.write(returnValue.join('\r\n'))
+  ];
+  socket.write(returnValue.join('\r\n'));
 }
 
-listen(80)
+listen(80);
